5d41f22d74f2da9217411be645815c9906fcb1b0
Gaurav Dhir <mail2gauravdhir@gmail.com>
2021-08-15 21:12:17 -0400

    Adding changes to enable react router for single page application

--- Project_1/confusion/src/App.js
+++ Project_1/confusion/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css'
 
 import { Navbar, NavbarBrand } from 'reactstrap';
@@ -10,9 +11,11 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
